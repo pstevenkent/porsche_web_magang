1245d9fdd0e40ff71b37673cb662255a396d6943
Patrickstvn <168065344+Patrickstvn@users.noreply.github.com>
2025-08-08 11:44:55 +0700

    catalog
    catalog

---
+++ public/data/CarData.js
@@ -0,0 +1,46 @@
+// src/data/carData.js
+
+// --- Sumber Data Mobil ---
+export const carData = {
+    "718": [
+      { id: 1, name: "718 Cayman", price: "Rp 3.000.000.000", image: "/images/cars/718-cayman.png" },
+      { id: 2, name: "718 Boxster", price: "Rp 3.100.000.000", image: "/images/cars/718-boxster.png" },
+    ],
+    "911": [
+      { id: 3, name: "911 Carrera", price: "Rp 4.300.000.000", image: "/images/cars/911-carrera.png" },
+      { id: 4, name: "911 Turbo S", price: "Rp 7.800.000.000", image: "/images/cars/911-turbo.png" },
+      { id: 5, name: "911 GT3 RS", price: "Rp 9.000.000.000", image: "/images/cars/911-gt3.png" },
+    ],
+    "Taycan": [
+      { id: 6, name: "Taycan 4S", price: "Rp 2.800.000.000", image: "/images/cars/taycan-4s.png" },
+    ],
+    "Panamera": [
+       { id: 7, name: "Panamera", price: "Rp 3.800.000.000", image: "/images/cars/panamera.png" },
+    ],
+    "Macan": [
+      { id: 8, name: "Macan", price: "Rp 2.100.000.000", image: "/images/cars/macan.png" },
+    ],
+    "Cayenne": [
+      { id: 9, name: "Cayenne", price: "Rp 2.800.000.000", image: "/images/cars/cayenne.png" },
+    ]
+  };
+  
+  // --- Data Halaman Konfigurator ---
+  export const configuratorDetails = {
+      name: "Macan",
+      year: "2025",
+      images: [
+          "/images/cars/macan-main.png",
+          "/images/cars/macan-side.png",
+          "/images/cars/macan-back.png",
+          "/images/cars/macan-front.png",
+          "/images/cars/macan-top.png",
+          "/images/cars/macan-wheel.png",
+          "/images/cars/macan-interior.png",
+      ],
+      colors: {
+          Contrasts: [{name: 'White', hex: '#F5F5F5'}, {name: 'Black', hex: '#1C1C1C'}],
+          Shades: [{name: 'Jet Black Metallic', hex: '#2A2A2A'}, {name: 'Volcano Grey', hex: '#4B4F52'}, {name: 'Dolomite Silver', hex: '#A9AEB2'}, {name: 'Genting Blue', hex: '#637A8A'}],
+          Dreams: [{name: 'Carmine Red', hex: '#C40000', price: '+ Rp 50.000.000'}, {name: 'Chalk', hex: '#D5D2C7', price: '+ Rp 50.000.000'}],
+      }
+  };
